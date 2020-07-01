1eb285666a881d99047949f4428d6daeed9e7d17
Edvaldo Nascimento <aedvaldo.nascimento@gmail.com>
2020-07-01 10:56:26 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/edvaldo/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.edvaldo.cursomc.services;
+
+import com.edvaldo.cursomc.domain.Categoria;
+import com.edvaldo.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria buscar(Integer id) {
+
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+        return categoria.orElse(null);
+    }
+}
