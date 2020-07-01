9ccac63c16d7a9774915b18276e270742c02a186
Edvaldo Nascimento <aedvaldo.nascimento@gmail.com>
2020-07-01 09:23:51 -0300

    Banco de Dados H2 e criacao automatica da base de dados

--- src/main/java/com/edvaldo/cursomc/domain/Categoria.java
+++ src/main/java/com/edvaldo/cursomc/domain/Categoria.java
@@ -1,4 +1,62 @@
 package com.edvaldo.cursomc.domain;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = -4336747825155732785L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id) &&
+                Objects.equals(nome, categoria.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/edvaldo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/edvaldo/cursomc/resources/CategoriaResource.java
@@ -1,4 +1,27 @@
 package com.edvaldo.cursomc.resources;
 
+import com.edvaldo.cursomc.domain.Categoria;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.add(cat1);
+        categorias.add(cat2);
+
+        return categorias;
+    }
 }
